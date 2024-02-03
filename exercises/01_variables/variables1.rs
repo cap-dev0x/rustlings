2ff9996a034f64619cbed4a73cfaade4c6f0c4b0
cap-dev0x <158111888+cap-dev0x@users.noreply.github.com>
2024-02-02 18:57:26 -0800

    fix(variables1.rs): answer for variables1.rs
    Signed-off-by: cap-dev0x <158111888+cap-dev0x@users.noreply.github.com>

--- exercises/01_variables/variables1.rs
+++ exercises/01_variables/variables1.rs
@@ -5,9 +5,10 @@
 // Execute `rustlings hint variables1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 fn main() {
-    x = 5;
+    //can fix by adding the declarative "let" keyword before declaration
+    let x = 5;
     println!("x has the value {}", x);
 }
