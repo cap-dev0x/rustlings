cde4182e0cb4e348ee2056b7473f08ed32e51023
cap-dev0x <158111888+cap-dev0x@users.noreply.github.com>
2024-02-02 19:06:45 -0800

    fix(variables3.rs): solution for variables3.rs
    Signed-off-by: cap-dev0x <158111888+cap-dev0x@users.noreply.github.com>

--- exercises/01_variables/variables3.rs
+++ exercises/01_variables/variables3.rs
@@ -3,9 +3,10 @@
 // Execute `rustlings hint variables3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 fn main() {
-    let x: i32;
+    //can fix by completing the statement with any singed int
+    let x: i32 = 2;
     println!("Number {}", x);
 }
